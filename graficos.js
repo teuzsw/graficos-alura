d420daf3f702e6a8acc9ef487d2a56bb693162eb
mateuscamargofernandes <128862400+teuzsw@users.noreply.github.com>
2024-10-30 15:08:39 -0300

    Update graficos.js

--- graficos.js
+++ graficos.js
@@ -1,3 +1,9 @@
 const url = "https://raw.githubusercontent.com/teuzsw/graficos-alura/refs/heads/main/dados-globais.json"
 
-async = 
+async function = visualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+}
+
+visualizarInformacoesGlobais()
