30909a3260407c25e9589e92f6a796971003a40e
mateuscamargofernandes <128862400+teuzsw@users.noreply.github.com>
2024-10-30 15:17:05 -0300

    Update graficos.js

--- graficos.js
+++ graficos.js
@@ -3,12 +3,9 @@ const url = "https://raw.githubusercontent.com/teuzsw/graficos-alura/refs/heads/
 async function = visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const paragrafo document.createElement('p')
-    const container = document.getElementById('graficos-container')
-    container.appendchild(paragrafo)
+    console.log(dados);
+    const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-   paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.Fabebook} de usuários no Facebook?`
-    console.log(paragrafo);
 }
 
 visualizarInformacoesGlobais()
