936cfbaf951eaf15eeab8638d6f0093fd27f2fbc
mateuscamargofernandes <128862400+teuzsw@users.noreply.github.com>
2024-10-30 15:14:34 -0300

    Update graficos.js

--- graficos.js
+++ graficos.js
@@ -4,6 +4,8 @@ async function = visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     const paragrafo document.createElement('p')
+    const container = document.getElementById('graficos-container')
+    container.appendchild(paragrafo)
     paragrafo.classList.add('graficos-container__texto')
    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.Fabebook} de usuários no Facebook?`
     console.log(paragrafo);
