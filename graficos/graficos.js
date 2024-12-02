843ce09647d14520d215b35d08ac5166ab1930a4
mateuscamargofernandes <128862400+teuzsw@users.noreply.github.com>
2024-12-02 08:27:26 -0300

    Update graficos.js

--- graficos/graficos.js
+++ graficos/graficos.js
@@ -4,19 +4,6 @@ async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
 
-    // Função para formatar números grandes
-    function formatarNumero(num) {
-        if (num >= 1e9) {
-            return (num / 1e9).toFixed(1) + ' bilhões'; // Ex: 33e9 -> 33 bilhões
-        } else if (num >= 1e6) {
-            return (num / 1e6).toFixed(1) + ' milhões'; // Ex: 33e6 -> 33 milhões
-        } else if (num >= 1e3) {
-            return (num / 1e3).toFixed(1) + ' mil'; // Ex: 33e3 -> 33 mil
-        } else {
-            return num; // Para números menores que mil, exibe o número diretamente
-        }
-    }
-
     const pessoasConectadas = formatarNumero(dados.total_pessoas_conectadas);
     const pessoasNoMundo = formatarNumero(dados.total_pessoas_mundo);
 
